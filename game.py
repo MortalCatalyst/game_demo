d9b7803c90b5ee4a923fff1aa5fe519bb9b1d7b7
mortalcatalyst <saythjwrenshaw@outlook.com>
2016-11-28 22:46:41 +1100

    complete

--- game.py
+++ game.py
@@ -1,13 +1,39 @@
+import os
+
+import time
+import random
+import colorama
+import sys
+
 
 class Game:
     def __init__(self):
-        pass
+        self.history = []
+        self.plays = [
+            (colorama.Fore.RED + 'Red', 'r'),
+            (colorama.Fore.YELLOW + 'Yellow', 'y'),
+            (colorama.Fore.GREEN + 'Green', 'g'),
+            (colorama.Fore.BLUE + 'Blue', 'b')
+        ]
 
     def show_level(self):
-        pass
+        self.clear()
+        for h in self.history:
+            print(h[0], end='  ')
+            sys.stdout.flush()
+            time.sleep(1)
+        self.clear()
 
     def add_move(self):
-        pass
+        self.history.append(random.choice(self.plays))
 
     def test_player(self):
-        pass
+        print(colorama.Fore.WHITE + "{} moves:".format(len(self.history)))
+        for t, v in self.history:
+            guess = input("Next [r,g,b,y]: ")
+            if guess != v:
+                return False
+        return True
+
+    def clear(self):
+        os.system('clear')
